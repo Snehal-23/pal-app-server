6913acd90013e73747fe48de7f639e777757fd1c
Snehal-23 <snehal.bodekar@outlook.com>
2022-07-08 16:47:38 +0530

    Webpack n procfile

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ app.get(`/`, (req, res) => {
 
 //prduction
 var server = app.listen(process.env.PORT || 3000, function () {
-  var port = server.address().port;
-  console.log("express is working");
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
